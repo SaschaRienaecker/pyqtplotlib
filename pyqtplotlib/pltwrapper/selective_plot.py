19c784b20650c3226aa46ef298f278195fbf7bb1
Sascha <rienaecker.sascha@t-online.de>
2023-11-14 10:41:01 +0100

    develop the SelectivePlotWidget() to improve the egonomicity of roi selection (using "E" + left mouse)

--- pyqtplotlib/pltwrapper/selective_plot.py
+++ pyqtplotlib/pltwrapper/selective_plot.py
@@ -1,7 +1,8 @@
+#%%
 import pyqtgraph as pg
 import numpy as np
 
-
+from PyQt5 import QtCore, QtWidgets
 from pyqtplotlib.pltwrapper import AxesWidget
 
 
@@ -19,11 +20,14 @@ class SelectivePlotWidget(AxesWidget):
         self.addItem(self.roi)
 
         # Hide ROI until Ctrl is pressed
-        # self.roi.hide()
-        self.ctrlPressed = False
-
-        # Connect ROI event
-        self.roi.sigRegionChanged.connect(self.onROIChanged)
+        self.roi.hide()
+        # self.ctrlPressed = False
+        self.ePressed = False
+        self.shiftPressed = False
+        self.roiStart = None
+        
+        # Connect ROI event with additional argument using lambda
+        self.roi.sigRegionChangeFinished.connect(lambda: self.onROIChanged(inverted=False))
 
     def set_data(self, x, y, **plot_kwargs):
         self.data = (np.array(x), np.array(y))
@@ -34,74 +38,81 @@ class SelectivePlotWidget(AxesWidget):
         return self.scatter
     
     
-    def _roi_mask(self):
-        
-        if self.data is None:
-            return None
-        
-        X, Y = self.data
-                
-        # mask = np.array([self.roi.contains(pg.Point(x, y)) for x,y in zip(X,Y)])
-        
-        bounds = self.roi.parentBounds()
-        x1, y1, x2, y2 = bounds.left(), bounds.top(), bounds.right(), bounds.bottom()
-        
-        mask = (X>x1) & (X<x2) & (Y>y1) & (Y<y2)
-        
-        return mask
-        
+
     def onROIChanged(self):
-        if self.scatter is None:
-            return
+        """This method is called when the ROI is changed. Override this method to add custom functionality."""
+        pass        
         
-        X, Y = self.data
+    def keyPressEvent(self, event):
+        if event.key() == QtCore.Qt.Key_E:
+            self.ePressed = True
+            
+        super().keyPressEvent(event)
+
+    def keyReleaseEvent(self, event):
+        if event.key() == QtCore.Qt.Key_E:
+            self.ePressed = False
+
+        super().keyReleaseEvent(event)
+
+    def mousePressEvent(self, event):
+        if self.ePressed:
+            self.roi.show()
+            self.roiStart = self.plotItem.vb.mapSceneToView(event.pos())
+            self.roi.setPos(self.roiStart, update=False)
+            self.roi.setSize([0, 0], update=False)
+        else:
+            super().mousePressEvent(event)
+
+    def mouseMoveEvent(self, event):
+        if self.ePressed and self.roiStart is not None:
+            currentPos = self.plotItem.vb.mapSceneToView(event.pos())
+            self.roi.setSize(currentPos - self.roiStart)
+        else:
+            super().mouseMoveEvent(event)
+
+    def mouseReleaseEvent(self, event):
+        if self.ePressed:
+            self.roi.hide()
+            self.roiStart = None
+        else:
+            super().mouseReleaseEvent(event)
         
-        mask = self._roi_mask()
-        if self.mask is None:
-            self.mask = mask
-        else: # append to existing mask
-            self.mask = np.hstack([self.mask, mask])
-        
-        
-        # alpha = [0.2 if m else 1 for m in mask]
-        # colors = [pg.mkColor((0, 0, 255, 255 * a)) for a in alpha]
-        # self.scatter.setBrush(colors)
-        # Placeholder for fitting functionality. Only fit data outside the ROI.
-        # self.apply_fit([x[i] for i, m in enumerate(mask) if not m],
-                    #    [y[i] for i, m in enumerate(mask) if not m])
-
-        self.set_data(X[~mask], Y[~mask])
-        
-
-    # def keyPressEvent(self, event):
-    #     if event.key() == QtCore.Qt.Key_Control:
-    #         self.ctrlPressed = True
-    #         self.roi.show()
-
-    # def keyReleaseEvent(self, event):
-    #     if event.key() == QtCore.Qt.Key_Control:
-    #         self.ctrlPressed = False
-    #         self.roi.hide()
-
-    # def mousePressEvent(self, event):
-    #     if self.ctrlPressed:
-    #         self.roi.show()
-    #         pos = self.plotItem.vb.mapSceneToView(event.pos())
-    #         self.roi.setPos(pos.x(), pos.y(), update=True, finish=False)
-    #         self.roi.setSize([0, 0])
-    #     super().mousePressEvent(event)
-
-    # def mouseMoveEvent(self, event):
-    #     if self.ctrlPressed:
-    #         pos = self.plotItem.vb.mapSceneToView(event.pos())
-    #         self.roi.setSize([pos.x() - self.roi.pos().x(), pos.y() - self.roi.pos().y()])
-    #     super().mouseMoveEvent(event)
 
 
 if __name__ == "__main__":
 
     from PyQt5.QtWidgets import QApplication, QMainWindow
     import sys
+    
+    # crate a classs that inherits from SelectivePlotWidget
+    # and add some functionality to the onROIChanged method;
+    # Here, we exclude the data points that are inside the ROI:
+    class ExcludeSelectionPlot(SelectivePlotWidget):
+        def __init__(self, parent=None):
+            super().__init__(parent)
+            
+            self.mask = None
+            
+        def onROIChanged(self):
+            bounds = self.roi.parentBounds()
+            x1, y1, x2, y2 = bounds.left(), bounds.top(), bounds.right(), bounds.bottom()
+            
+            x, y = self.data    
+            mask = (x>x1) & (x<x2) & (y>y1) & (y<y2)
+            self.update_mask(mask)
+        
+        def update_mask(self, mask):
+            
+            if self.mask is None:
+                self.mask = mask
+            else:
+                self.mask = np.hstack([self.mask, mask]) # append to existing mask
+            x, y = self.data
+            self.set_data(x[~mask], y[~mask])
+        
+    
+    app = 0
     app = QApplication(sys.argv)
     window = QMainWindow()
 
@@ -109,8 +120,9 @@ if __name__ == "__main__":
     import signal
     signal.signal(signal.SIGINT, signal.SIG_DFL)
 
-    # Create a custom plot widget
-    ax = SelectivePlotWidget()
+    
+    ax = ExcludeSelectionPlot()  
+    
 
     # Add the plot widget to the main window
     window.setCentralWidget(ax)
@@ -123,3 +135,4 @@ if __name__ == "__main__":
 
     window.show()
     sys.exit(app.exec_())
+# %%
